caa30ccdb641a4c20a123d5efc879c7535ec2f1b
alshehata <alshehata@uplandsoftware.com>
2020-11-06 14:57:54 +0200

    [HR Task Project] adding the presentation layer

---
+++ Sahab.Enterprise.HR/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Sahab.Enterprise.HR.MvcApplication" Language="C#" %>
---
+++ Sahab.Enterprise.HR/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Sahab.Enterprise.HR.Startup))]
+namespace Sahab.Enterprise.HR
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
